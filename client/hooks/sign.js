f15537088132704ec09cf62a6446a72f9eabd95c
gorkem <gorkem.kockaya@gmail.com>
2020-06-15 20:53:21 +0300

    refactored client/signup, added client/signin

---
+++ client/hooks/sign.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import Router from 'next/router';
+
+import useRequest from './use-request';
+
+export default ({ url, text }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const { doRequest, errors } = useRequest({
+    url,
+    method: 'post',
+    body: { email, password },
+    onSuccess: () => Router.push('/')
+  });
+
+  const onSubmit = e => {
+    e.preventDefault();
+    doRequest();
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <h1>{text}</h1>
+      <div className='form-group'>
+        <label>Email Address</label>
+        <input
+          className='form-control'
+          onChange={e => setEmail(e.target.value)}
+        />
+      </div>
+      <div className='form-group'>
+        <label>Password</label>
+        <input
+          type='password'
+          className='form-control'
+          onChange={e => setPassword(e.target.value)}
+        />
+      </div>
+      {errors}
+      <button className='btn btn-primary'>Sign Up</button>
+    </form>
+  );
+};
--- client/pages/auth/signin.js
+++ client/pages/auth/signin.js
@@ -1,5 +1,5 @@
 import Sign from '../../hooks/sign';
 
 export default () => {
-  return <Sign url='/api/users/signup' text='Sign Up' />;
+  return <Sign url='/api/users/signin' text='Sign In' />;
 };
